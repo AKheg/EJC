da8becea29e6c30ffdd6f13eb7b21ad8899ffbbe
Артём <artem.hegay@gmail.com>
2017-06-28 23:46:55 +0300

    massive of ducks initialization added

--- task_2/Main.java
+++ task_2/Main.java
@@ -2,9 +2,25 @@
 public class Main {
 
     public static void main(String[] args) {
+        Duck[] ducks = new Duck[5];
+        for (int i = 0; i < 5; i++){
+            Duck duck;
+            int rnd = (int)(Math.random() * 3);
+            switch(rnd){
+                case 0: duck = new RubberDuck(); break;
+                case 1: duck = new MallardDuck(); break;
+                case 2: duck = new DarkwingDuck(); break;
+                default: duck = new DefaultDuck(); break;
+            }
+            ducks[i] = duck;
+            ducks[i].performFly();
+        }
+
      MallardDuck MD = new MallardDuck();
      MD.performFly();
      int a = MD.getSpeed();
-        System.out.println(a);
+     for (int i = 0; i < 5; i++){
+         System.out.println(ducks[i].getSpeed());
+     }
     }
 }
