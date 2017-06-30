8d27c6fe86f1224f6624042bba7949324803c0e1
Артём <artem.hegay@gmail.com>
2017-06-30 13:51:43 +0300

    Created new class, because of strong logic in Main class

--- task_2/Main.java
+++ task_2/Main.java
@@ -1,56 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Welcome to DuckCasino1");
-
-        int Cash = 500;
-        while (Cash > 0) {
-            System.out.println("choose your duck from 0 to 4:");
-
-            int chosenOne;
-            Scanner in = new Scanner(System.in);
-            chosenOne = in.nextInt();
-
-            Duck[] ducks = new Duck[5];
-            for (int i = 0; i < 5; i++) {
-                Duck duck;
-                int rnd = (int) (Math.random() * 3);
-                switch (rnd) {
-                    case 0:
-                        duck = new RubberDuck();
-                        break;
-                    case 1:
-                        duck = new MallardDuck();
-                        break;
-                    case 2:
-                        duck = new DarkwingDuck();
-                        break;
-                    default:
-                        duck = new DefaultDuck();
-                        break;
-                }
-                ducks[i] = duck;
-                ducks[i].performFly();
-            }
-
-            int speedMax = 0;
-
-            for (int i = 0; i < 5; i++) {
-                int CurrentSpeed = ducks[i].flyBehavior.getSpeed();
-                if (speedMax < CurrentSpeed) {
-                    speedMax = CurrentSpeed;
-                }
-            }
-            if (ducks[chosenOne].flyBehavior.getSpeed() < speedMax) {
-                System.out.println("you lost!");
-                Cash -= 200;
-            } else {
-                System.out.println("Congratulations! You win!");
-                Cash += 200;
-            }
-            System.out.println("Your balance now: "+Cash);
-        }
+        DuckRace dr = new DuckRace();
+        dr.run();
     }
 }
