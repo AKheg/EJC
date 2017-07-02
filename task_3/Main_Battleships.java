0307a4749ad794ee1b1a3038d94388bede8c717e
Артём <artem.hegay@gmail.com>
2017-07-02 03:02:56 +0300

    task_3: wrote a method which randomly puts 4-deck ship on the field, run some tests

---
+++ task_3/Main_Battleships.java
@@ -0,0 +1,13 @@
+public class Main_Battleships {
+    public static void main(String[] args) {
+        Battleships bs = new Battleships();
+        int[][] field = bs.Field;
+        bs.Put4PShip(field);
+        for (int[] x : field){
+            for (int y : x){
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+    }
+}
