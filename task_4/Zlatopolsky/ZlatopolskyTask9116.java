4b8b9858a8bb7a321877925ef5e7397d57ecd6a7
Артём <artem.hegay@gmail.com>
2017-07-10 23:44:34 +0300

    renamed classes and arguments by Code Conventions

---
+++ task_4/Zlatopolsky/ZlatopolskyTask9116.java
@@ -0,0 +1,20 @@
+package Zlatopolsky;
+
+public class ZlatopolskyTask9116 {
+    public static void main(String[] args) {
+        String firstString = "АРГЕНТИНА МАНИТ НЕГРА";
+        String secondString = "ПОТ КАК ПОТОП";
+        String thirdString = "А РОЗА УПАЛА НА ЛАПУ АЗОРА";
+        System.out.println(palindrome(firstString));
+        System.out.println(palindrome(secondString));
+        System.out.println(palindrome(thirdString));
+    }
+    private static boolean palindrome(String stringArg){
+        stringArg = stringArg.replaceAll(" ", "");
+        char[] charStringArg = stringArg.toCharArray();
+        for (int i = 0; i < stringArg.length() / 2 + 1; i++) {
+            if (!(charStringArg[i] == charStringArg[stringArg.length() - i - 1])) return false;
+        }
+        return true;
+    }
+}
--- task_4/Zlatopolsky/ZlatopolskyTask943.java
+++ task_4/Zlatopolsky/ZlatopolskyTask943.java
@@ -4,14 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class task_9_43 {
+public class ZlatopolskyTask943 {
 
     public static void main(String args[]){
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
-            String str = (reader.readLine());
-            char[] str2 = str.toCharArray();
-            for (int i = 0; i < str.length(); i += 2) {
-                System.out.print(str2[i]);
+            String input = reader.readLine();
+            char[] charInput = input.toCharArray();
+            for (int i = 0; i < input.length(); i += 2) {
+                System.out.print(charInput[i]);
             }
         } catch (IOException e){
             e.printStackTrace();
