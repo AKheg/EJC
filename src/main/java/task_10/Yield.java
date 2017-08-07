d3b863f022d59d6064db421f3f99b11b87f0f34c
Артём <artem.hegay@gmail.com>
2017-08-07 23:35:09 +0300

    join, wait, notify demonstration

---
+++ src/main/java/task_10/Yield.java
@@ -0,0 +1,17 @@
+package task_10;
+
+public class Yield {
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.err.println(Thread.currentThread().getName() + " " + i);
+                Thread.yield();
+            }
+        };
+
+        new Thread(runnable).start();
+        new Thread(runnable).start();
+        new Thread(runnable).start();
+        new Thread(runnable).start();
+    }
+}
